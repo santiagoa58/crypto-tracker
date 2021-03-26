aa08fa89ca851f1ed1882a8b4cc5b1348043c991
Santi <24705975+santiagoa58@users.noreply.github.com>
2021-03-26 18:21:31 -0400

    add initial color scheme

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,6 +1,13 @@
+//https://www.material.io/resources/color/#!/?view.left=1&view.right=0&primary.color=212121&secondary.color=1976D2&primary.text.color=EEEEEE&secondary.text.color=ffffff
 export const colors = {
-  primary: "black",
-  secondary: "white",
+  primary: "#212121",
+  primaryLight: "#484848",
+  primaryDark: "#000000",
+  fontPrimary: "#ededed",
+  secondary: "#1976d2",
+  secondaryLight: "#63a4ff",
+  secondaryDark: "#004ba0",
+  fontSecondaryLight: "#000000",
 };
 
 export const theme = {
